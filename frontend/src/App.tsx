37c419407bac6ec8c6bc7157277380bac54c1f19
Jad alKhawand <jkh42005@gmail.com>
2024-03-01 01:08:40 +0200

    Job page is done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,40 @@
 import { useContext, lazy, Suspense } from "react";
 import { ThemeContext } from "./context/theme.context";
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/Navbar.components";
 import CustomLinearProgress from "./components/navbar/custom-linear-progress/CustomLinearProgress.component";
 import AddCompany from "./pages/companies/AddCompany";
+import AddJob from "./pages/jobs/AddJob.page";
 
 // import with lazy loading
-const Home = lazy(()=> import("./pages/home/home.page"))
-const Companies = lazy(()=> import("./pages/companies/Companies.page"))
+const Home = lazy(() => import("./pages/home/home.page"));
+const Companies = lazy(() => import("./pages/companies/Companies.page"));
+const Jobs = lazy(() => import("./pages/jobs/Jobs.page"));
 const App = () => {
-  const {darkMode} = useContext(ThemeContext);
-  const appStyles = darkMode ? "app dark" : "app"
+  const { darkMode } = useContext(ThemeContext);
+  const appStyles = darkMode ? "app dark" : "app";
   return (
     <div className={appStyles}>
       <h1>Navbar</h1>
-      <Navbar/>
+      <Navbar />
       <div className="wrapper">
-        <Suspense fallback={<CustomLinearProgress/>}>
+        <Suspense fallback={<CustomLinearProgress />}>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/companies">
+              <Route index element={<Companies />} />
+              <Route path="add" element={<AddCompany />} />
+            </Route>
 
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/companies">
-          <Route index element={<Companies />}/>
-          <Route path="add" element={<AddCompany />} />
-          </Route>
-        </Routes>
+            <Route path="/jobs">
+              <Route index element={<Jobs />} />
+              <Route path="add" element={<AddJob />} />
+            </Route>
+          </Routes>
         </Suspense>
       </div>
     </div>
-  )
+  );
 };
 
 export default App;
